ec36fbcd083e1b378f101269cf94f34a3b1e04e6
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-01-16 13:28:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  var [inputText, setInputText] = useState("");
+
+  function inputChangeHandler(event) {
+    setInputText(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Inside out</h1>
       </header>
+      <input className="input" onChange={inputChangeHandler}></input>
+      Welcome {inputText}
     </div>
   );
 }
