754af4c02643dffd09101bb79db0734a4e03282a
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-01-20 13:33:58 +0530

    added emoji pad

--- src/App.js
+++ src/App.js
@@ -5,41 +5,51 @@ function App() {
   var [meaning, setMeaning] = useState("");
 
   var emojiDictionary = {
-    "🙈" : "See-No-Evil Monkey",
-    "🙉" : "Hear-No-Evil Monkey",
-    "🙊" : "Speak-No-Evil Monkey",
-    "💥" : "Collision",
-    "💫" : "Dizzy",
-    "💦" : "Sweat Droplets",
-    "💨" : "Dashing Away",
-    "🐵" : "Monkey Face",
-    "🐒" : "Monkey",
-    "🦍" : "Gorilla",
-    "🦧" : "Orangutan",
-    "🐶" : "Dog Face",
-    "🐕" : "Dog",
-    "🌹" : "Rose",
-    "🥀" : "Wilted Flower",
-    "🌺" : "Hibiscus",
-    "🌻" : "Sunflower",
-    "🌼" : "Blossom",
-    "🌷" : "Tulip",
-    "🌱" : "Seedling",
-    "🌲" : "Evergreen Tree",
-    "🌳" : "Deciduous Tree",
-    "🌴" : "Palm Tree",
-    "🌵" : "Cactus",
-    "🌾" : "Sheaf of Rice",
-    "🌿" : "Herb",
-    "☘️" : "Shamrock",
-    "🍀" : "Four Leaf Clover"
+    "🙈": "See-No-Evil Monkey",
+    "🙉": "Hear-No-Evil Monkey",
+    "🙊": "Speak-No-Evil Monkey",
+    "💥": "Collision",
+    "💫": "Dizzy",
+    "💦": "Sweat Droplets",
+    "💨": "Dashing Away",
+    "🐵": "Monkey Face",
+    "🐒": "Monkey",
+    "🦍": "Gorilla",
+    "🦧": "Orangutan",
+    "🐶": "Dog Face",
+    "🐕": "Dog",
+    "🌹": "Rose",
+    "🥀": "Wilted Flower",
+    "🌺": "Hibiscus",
+    "🌻": "Sunflower",
+    "🌼": "Blossom",
+    "🌷": "Tulip",
+    "🌱": "Seedling",
+    "🌲": "Evergreen Tree",
+    "🌳": "Deciduous Tree",
+    "🌴": "Palm Tree",
+    "🌵": "Cactus",
+    "🌾": "Sheaf of Rice",
+    "🌿": "Herb",
+    "☘️": "Shamrock",
+    "🍀": "Four Leaf Clover",
+    "": "",
   };
+
+  var weKnowEmoji = Object.keys(emojiDictionary);
 
   function inputChangeHandler(event) {
     var inputText = event.target.value;
     var meaning = emojiDictionary[inputText];
-    setMeaning(meaning)
-  
+    if (meaning === undefined) {
+      meaning = "we don't have this in our dictionary";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
   }
 
   return (
@@ -49,6 +59,16 @@ function App() {
       </header>
       <input className="input" onChange={inputChangeHandler}></input>
       <h2>{meaning}</h2>
+      <h3>Emoji Pad</h3>
+      {weKnowEmoji.map((emoji) => {
+        return (
+          <span  onClick={() => emojiClickHandler(emoji)} 
+          key={emoji}>
+
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
